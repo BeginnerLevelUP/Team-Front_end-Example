dadeb0742ec468dcf3a0a0d9251867fd78955376
Damion Morgan <only4school5680@gmail.com>
2024-07-03 18:01:56 -0400

    fixed the background image so it looks like figma file on both desktop and mobile

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,12 @@
 export default function Home() {
   return (
     <>
-    <h1>Home Page</h1>
+    {/* Container */}
+    <div className="w-screen h-screen">
+      {/* Banner */}
+      <section className="bg-[url('../public/homePageAssests/hero-img.png')] lg:h-full h-1/2 bg-no-repeat bg-cover lg:bg-top  lg:bg-cover lg:bg-100% bg-[75%]">
+      </section>
+    </div>
     </>
   );
 }
