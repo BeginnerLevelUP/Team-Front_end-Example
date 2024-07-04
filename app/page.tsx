900f54632a5c2698f94dd2eb6e4dd457c15c6b7b
Damion Morgan <only4school5680@gmail.com>
2024-07-04 16:36:45 -0400

    finished top half of banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,33 @@
 
-
+import burger from "@/public/homePageAssests/burger.png"
+import Image from "next/image";
 export default function Home() {
   return (
     <>
     {/* Container */}
     <div className="w-screen h-screen">
       {/* Banner */}
-      <section className="bg-[url('../public/homePageAssests/hero-img.png')] lg:h-full h-1/2 bg-no-repeat bg-cover lg:bg-top  lg:bg-cover lg:bg-100% bg-[75%] flex flex-col p-10">
+      <section className="bg-[url('../public/homePageAssests/hero-img.png')] lg:h-full h-1/2  bg-no-repeat bg-cover lg:bg-top  lg:bg-cover lg:bg-100% bg-[85%] flex flex-col p-10">
 
-        {/* Top Half */}
-        <div className="flex justify-evenly "> 
-        <p className="text-6xl text-white">team<span className="text-[#5468E7]">.</span></p>
-
-        <nav className="flex justify-between items-center list-none underline text-[#d3d3d3] text-2xl">
+        {/* Desktop Top Half */}
+        <div className="lg:flex hidden justify-end">
+          <p className="text-6xl text-white mr-96">team<span className="text-[#5468E7]">.</span></p>
+          <nav className="flex justify-between items-center list-none underline text-[#d3d3d3] text-2xl mx-24">
           <ul className="flex">
-            <li className="mr-6">Product</li>
-            <li className="mr-6">Blog</li>
-            <li className="mr-6">Support</li>
-          </ul>
-          <ul className="flex items-center">
-            <li className="mr-6">Login</li>
-            <li>
-              <button className="border border-[#5468E7] bg-[#5468E7] bg-opacity-50 rounded px-5 py-2">Get Access</button>
-            </li>
+            <li className="mx-6">Product</li>
+            <li className="mx-6">Blog</li>
+            <li className="mx-6">Support</li>
+            <li className="mx-6">Login</li>
           </ul>
         </nav>
 
-        
+        <button className="border border-[#5468E7] bg-[#5468E7] bg-opacity-50 rounded px-20 py-2  text-[#d3d3d3] ml-24">Get Access</button>
+        </div>
+
+        {/* Mobile Top Half */}
+        <div className="flex justify-between">
+          <p className="text-3xl text-white">team<span className="text-[#5468E7]">.</span></p>
+          <Image src={burger} alt='burger menu icon' className=""></Image>
         </div>
 
 
